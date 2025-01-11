d277ddef403384f7ede34a62df77673bc826b4b6
SMKim94 <floor3th@gmail.com>
2025-01-12 06:20:58 +0900

    Playwright 설정 수정

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig, devices } from '@playwright/test';
+import path from 'path';
+import './env.config.ts';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-import { loadEnvConfig } from '@next/env';
-
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
+const testDir = path.resolve(
+  process.cwd(),
+  process.env.PLAYWRIGHT_BASE_DIR ?? '',
+);
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests/e2e',
+  testDir: testDir,
+
+  testMatch: '**/*.e2e.ts',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,20 +24,20 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
 
-  outputDir: './tests/e2e/test-results',
+  outputDir: path.resolve(testDir, 'test-results'),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     [
       'html',
       {
-        outputFolder: './tests/e2e/playwright-report',
+        outputFolder: path.resolve(testDir, 'playwright-report'),
       },
     ],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -100,7 +101,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm run dev',
-    url: 'http://localhost:3000',
+    url: baseURL,
     reuseExistingServer: !process.env.CI,
   },
 });
