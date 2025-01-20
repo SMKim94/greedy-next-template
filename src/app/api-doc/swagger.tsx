a3f2576b886b7d847042629d618bf8f0b1229611
SMKim94 <floor3th@gmail.com>
2025-01-21 02:24:01 +0900

    any 대신 타입 지정

--- src/app/api-doc/swagger.tsx
+++ src/app/api-doc/swagger.tsx
@@ -2,11 +2,11 @@
 
 import { useEffect } from 'react';
 import { SwaggerUIBundle } from 'swagger-ui-dist';
+import type { Spec } from 'swagger-ui-dist';
 import 'swagger-ui-dist/swagger-ui.css';
 
 type Props = {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  spec: object;
+  spec: Spec;
 };
 
 const Swagger = ({ spec }: Props) => {
